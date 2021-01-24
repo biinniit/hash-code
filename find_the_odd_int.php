a23020ce7087dfe907c099744053f441fda3b888
Elvis <popup2elvis@gmail.com>
2021-01-24 17:10:28 +0100

    Create find_the_odd_int.php

---
+++ find_the_odd_int.php
@@ -0,0 +1,14 @@
+function findIt(array $seq) : int
+{
+  $mm = $seq;
+  foreach($seq as $se){
+    $orig_size = count($mm);
+    $ll = array_diff($mm, [$se]);
+    $new_size = count($ll);
+    $check_count = $orig_size - $new_size;
+    if($check_count % 2 != 0){
+      return $se;
+    }
+    $mm = $ll;    
+  }  // Enter your code here
+}
