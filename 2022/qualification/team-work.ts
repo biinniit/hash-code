447c5a6aa5ec26a742b0cf4279678a7da6f6cf53
Bennet Otoho <bennet.otoho@trilogy.com>
2022-02-28 03:41:28 +0100

    2022 qualification: practice submission, random contributors

--- 2022/qualification/team-work.ts
+++ 2022/qualification/team-work.ts
@@ -1,8 +1,17 @@
 /**
- * First submission that passed all test cases
- * Score: 1,967,136
+ * Practice submission after getting contributors randomly
+ * and checking the entire priority queue for each project
+ * and adjusting the weights depending on the test case
+ * 
+ * Optimal weights for the test cases:
+ * Default: DURATION_WEIGHT=5,SCORE_WEIGHT=1,EXPIRY_WEIGHT=5
+ * C: DURATION_WEIGHT=150,SCORE_WEIGHT=1,EXPIRY_WEIGHT=5
+ * F: DURATION_WEIGHT=1000,SCORE_WEIGHT=1,EXPIRY_WEIGHT=1
+ * 
+ * Still yet to implement mentorship logic
+ * 
+ * Score: 2,352,790
  */
-
 import * as fs from 'fs'
 
 type Skills = Record<string, number>
@@ -39,6 +48,12 @@ readInput(people, projects)
 //console.log('Projects (before sort):')
 //console.log(projects)
 
+let AVG_DURATION = 0
+let AVG_SCORE = 0
+let AVG_EXPIRY = 0
+let AVG_NUMBER_PEOPLE = 0
+getAverages()
+
 projects.sort(compareProjects)
 //console.log('Projects (after sort):')
 //console.log(projects)
@@ -46,63 +61,18 @@ projects.sort(compareProjects)
 const projectQueue: Project[] = []
 const assignments: Assignment[] = []
 
-for(let i = 0, tryAgain = false; i < projects.length; ++i) {
-  let project: Project
-  let incrementSkill: number[] = []
-  let incrementSkillFor: string[] = []
-  if(tryAgain) {
-    if(projectQueue.length !== 0) {
-      project = projectQueue[0]
-      //console.log(`Trying ${project.name} again...`)
-      --i
-    }
-    else {
-      tryAgain = !tryAgain
-      --i; continue
-    }
+for(let i = 0, j; i < projects.length; ++i) {
+  let assignment = getAssignmentFrom(projects[i])
+  if(assignment == null) {
+    projectQueue.push(projects[i])
   }
-  else
-    project = projects[i]
 
-  const assignment: Assignment = { project: project.name, people: [] }
-  for(const [role, level] of Object.entries(project.roles)) {
-    let foundPerson = undefined
-    for(let j = 0; j < people.length; ++j) {
-      const person = people[j]
-      if(role.trimEnd() in person.skills && person.skills[role.trimEnd()] >= level) {
-        foundPerson = person.name
-        if(assignment.people.includes(foundPerson)) {
-          foundPerson = undefined
-          continue
-        }
-        if(person.skills[role.trimEnd()] === level) {
-          incrementSkill.push(j)
-          incrementSkillFor.push(role.trimEnd())
-        }
-        break
-      }
-    }
-
-    if(foundPerson == null)
+  for(j = 0; j < projectQueue.length; ++j) {
+    assignment = getAssignmentFrom(projectQueue[j])
+    if(assignment == null)
       break
-    assignment.people.push(foundPerson)
   }
-
-  if(assignment.people.length === Object.keys(project.roles).length) {
-    if(!(tryAgain = !tryAgain))
-      projectQueue.shift()
-    //console.log(`Adding assignment for '${assignment.project}'`)
-    
-    assignments.push(assignment)
-
-    // increment skills
-    for(let index = 0; index < incrementSkill.length; ++index)
-      ++(people[incrementSkill[index]].skills[incrementSkillFor[index]])
-  }
-  else if(assignment.people.length <= Object.keys(project.roles).length) {
-    if(tryAgain = !tryAgain)
-      projectQueue.push(project)
-  }
+  projectQueue.splice(0, j)
 }
 //console.log('Assignments:')
 //console.log(assignments)
@@ -110,9 +80,9 @@ for(let i = 0, tryAgain = false; i < projects.length; ++i) {
 writeOutput(assignments)
 
 function calculatePriority(project: Project): number {
-  return ((maxExpiry - project.duration) * DURATION_WEIGHT
+  return ((AVG_DURATION - project.duration) * DURATION_WEIGHT
     + project.score * SCORE_WEIGHT
-    + (maxExpiry - project.expiry) * EXPIRY_WEIGHT)
+    + (AVG_EXPIRY - project.expiry) * EXPIRY_WEIGHT)
     / Object.keys(project.roles).length
 }
 
@@ -126,6 +96,76 @@ function compareProjects(a: Project, b: Project): number {
 
   //console.log(`${a.name} priority: ${aPriority}, ${b.name} priority: ${bPriority}`)
   return bPriority - aPriority
+}
+
+function getAverages() {
+  projects.forEach(project => {
+    AVG_DURATION += project.duration
+    AVG_SCORE += project.score
+    AVG_EXPIRY += project.expiry
+    AVG_NUMBER_PEOPLE += Object.keys(project.roles).length
+  })
+  AVG_DURATION /= projects.length; AVG_SCORE /= projects.length
+  AVG_EXPIRY /= projects.length; AVG_NUMBER_PEOPLE /= projects.length
+  console.log(`Average duration: ${AVG_DURATION}`)
+  console.log(`Average score: ${AVG_SCORE}`)
+  console.log(`Average expiry: ${AVG_EXPIRY}`)
+  console.log(`Average no. of people: ${AVG_NUMBER_PEOPLE}`)
+}
+
+function getAssignmentFrom(project: Project): Assignment | null {
+  let incrementSkill: number[] = []
+  let incrementSkillFor: string[] = []
+  let assignment: Assignment | null = { project: project.name, people: [] }
+
+  //console.log(`Attempting assignment for '${project.name}'`)
+  for(const [role, level] of Object.entries(project.roles)) {
+    let foundPerson = undefined
+    let randomPeople = [...people.keys()]
+    shuffle(randomPeople)
+    for(let j = 0; j < people.length; ++j) {
+      const person = people[randomPeople[j]]
+      if(role.trimEnd() in person.skills && person.skills[role.trimEnd()] >= level) {
+        foundPerson = person.name
+        if(assignment.people.includes(foundPerson)) {
+          foundPerson = undefined
+          continue
+        }
+        if(person.skills[role.trimEnd()] === level) {
+          incrementSkill.push(randomPeople[j])
+          incrementSkillFor.push(role.trimEnd())
+        }
+        break
+      }
+    }
+
+    if(foundPerson == null)
+      break
+    assignment.people.push(foundPerson)
+  }
+
+  if(assignment.people.length === Object.keys(project.roles).length) {
+    //console.log(`Adding assignment for '${assignment.project}'`)
+    assignments.push(assignment)
+
+    // increment skills
+    for(let index = 0; index < incrementSkill.length; ++index)
+      ++people[incrementSkill[index]].skills[incrementSkillFor[index]]
+  }
+  else if(assignment.people.length <= Object.keys(project.roles).length) {
+    //console.log(`Assignment for ${assignment.project} failed with people, `, assignment.people)
+    assignment = null
+  }
+
+  return assignment
+}
+
+function shuffle(indices: number[]) {
+  let i = indices.length, r
+  while(i != 0) {
+    r = Math.floor(Math.random() * i--);
+    [indices[i], indices[r]] = [indices[r], indices[i]]
+  }
 }
 
 function readInput(people: Person[], projects: Project[]) {
